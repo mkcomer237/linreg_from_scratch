0e9b5e599c9a153f29383f58abe0c9444a5dbf34
Max Comer <mkcomer@gmail.com>
2021-08-18 14:20:34 -0700

    first commit with initial linear_regression class

---
+++ README.md
@@ -0,0 +1,1 @@
+# linreg_from_scratch
---
+++ linear_regression.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class linear_regression(): 
+    """A linear regression class which takes in an X and y input, and calculates the 
+    least squares coefficients and other model statistics."""
+    # Initialize the class with X and y datasets 
+    def __init__(self, X, y): 
+        self.X = X 
+        self.y = y
+    
+    # Simple training using the normal equations derived through linear algebra 
+    def train(self):
+        X = self.X
+        y = self.y
+        self.b_hat = np.dot(np.linalg.inv(np.dot(X.T, X)), np.dot(X.T, y))
