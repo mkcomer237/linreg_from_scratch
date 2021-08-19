4175977529be1050507e4e6fc391f8df7772bce6
Max Comer <mkcomer@gmail.com>
2021-08-19 14:49:44 -0700

    add init file

--- linear_regression.py
+++ linear_regression.py
@@ -13,3 +13,14 @@ class linear_regression():
         X = self.X
         y = self.y
         self.b_hat = np.linalg.inv(X.T @ X) @ X.T @ y
+
+    def __str__(self):
+        # Print out the first 7 columns and the coefficients 
+        print_string = ['X' + '\t' + 'b_hat' + '\t' + 'y']
+        for i in range(min(len(self.y), 7)):
+            try: 
+                b_hat_clean = str(self.b_hat[i])
+            except:
+                b_hat_clean = ' '
+            print_string.append(str(self.X[i]) + '\t' + b_hat_clean + '\t' + str(self.y[i]).strip())
+        return '\n'.join(print_string)
