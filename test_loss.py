8db53feafd950021e7b338615e7365f7107f995c
mkcomer237 <mkcomer@gmail.com>
2021-10-22 10:02:25 +0200

    add MSE loss function

---
+++ test_loss.py
@@ -0,0 +1,12 @@
+from LossFunctions import MSE
+import numpy as np
+
+
+
+X = np.array([[1, 1, 1, 1], [0, 1, 3, 4]]).T
+y = np.array([0, 8, 8, 20]).reshape(4, 1)
+b_hat = np.array([2, 3]).reshape(2, 1)
+print('Shapes (X, b_hat):', X.shape, b_hat.shape)
+
+
+print(MSE(X, y, b_hat))
