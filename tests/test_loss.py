1dba986ea7c70dfe1eddd75abc4117e2819f7c9c
mkcomer237 <mkcomer@gmail.com>
2021-11-02 08:59:11 -0400

    update ignored files

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -1,7 +1,7 @@
 """Smaller set of tests for gradient descent."""
 
 import sys
-sys.path.append('/Users/maxcomer/Dropbox/Python Deep Learning/linear_regression/linear_regression_github') #noqa
+sys.path.append('/Users/maxcomer/Dropbox/Python Deep Learning/linear_regression/linear_regression_github')  # noqa
 from GDHelperFunctions import MSE, MSEGradient
 from LinearRegression import LinearRegression
 import numpy as np
@@ -9,11 +9,11 @@ import numpy as np
 
 X = np.array([0, 1, 3, 4]).reshape(4, 1)
 y = np.array([0, 8, 8, 20]).reshape(4, 1)
-#b_hat = np.array([2, 3]).reshape(2, 1)
-#print('Shapes (X, b_hat):', X.shape, b_hat.shape)
+# b_hat = np.array([2, 3]).reshape(2, 1)
+# print('Shapes (X, b_hat):', X.shape, b_hat.shape)
 
 
-#print(MSE(X, y, b_hat))
+# print(MSE(X, y, b_hat))
 
 reg = LinearRegression(X, y, prepend_ones=True)
 
